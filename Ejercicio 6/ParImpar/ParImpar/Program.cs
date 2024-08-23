1f8fe7312886e059fcd5b39374554dedf6f9beb2
unknown <fergomez@uamv.edu.ni>
2024-08-22 20:03:10 -0600

    all

---
+++ Ejercicio 6/ParImpar/ParImpar/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace ParImpar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            Console.Write("Digita un numero entero cualquiera: ");
+            num = int.Parse(Console.ReadLine());
+            if (num%2==0)
+            {
+                Console.Write($"El numero {num} es par");
+            }
+            else
+            {
+                Console.Write($"El numero {num} es impar");
+            }
+        }
+    }
+}
---
+++ Ejercicio 7/MayorNum/MayorNum/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace MayorNum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numeros = new int[3];
+
+            for (int i = 0; i < numeros.Length; i++)
+            {
+                Console.WriteLine($"Ingrese numero {i + 1}: ");
+                numeros[i] = int.Parse(Console.ReadLine());
+            }
+            int n = numeros.Length;
+            for (int i = 0; i < n - 1; i++)
+            {
+                for (int j = 0; j < n - 1 - i; j++)
+                {
+                    if (numeros[j] < numeros[j + 1])
+                    {
+                        int temp = numeros[j];
+                        numeros[j] = numeros[j + 1];
+                        numeros[j + 1] = temp;
+                    }
+                }
+            }
+
+            Console.WriteLine("El orden de mayor a menor es: ");
+            for (int i = 0; i < numeros.Length; i++)
+            {
+                Console.WriteLine($"{numeros[i]}");
+            }
+        }
+    }
+}
