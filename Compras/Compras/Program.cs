5784768c0613d3d8e9db800c27cb58deddfe3361
Fergago20 <fergomez@uamv.edu.ni>
2024-08-25 13:40:57 -0600

    po

---
+++ Compras/Compras/Program.cs
@@ -0,0 +1,32 @@
+﻿using Compras.clases;
+
+namespace Compras
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ejemplo 1");
+            Factura fact = new Factura();
+            string producto=null;
+            double precio=0, monto;
+            int cantidad=0;
+
+            Console.Write("Producto: ");
+            producto=Console.ReadLine();
+            Console.Write("Precio: ");
+            precio=double.Parse(Console.ReadLine());
+            Console.Write("Cantidad: ");
+            cantidad=int.Parse(Console.ReadLine());
+
+            fact.producto = producto;
+            fact.precio = precio;
+            fact.cantidad = cantidad;
+            monto = fact.calcularMonto();
+
+            Console.WriteLine($"\n{producto}");
+            Console.WriteLine($"Precio ${precio}");
+            Console.WriteLine($"monto ${monto}");
+        }
+    }
+}
---
+++ Compras/Compras/clases/Factura.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Compras.clases
+{
+    internal class Factura
+    {
+        public string producto { get; set; }
+        public double precio {  get; set; }
+        public int cantidad { get; set; }
+        public double calcularMonto()
+        {
+            return this.precio * this.cantidad;
+        }
+        public Factura() { }
+    }
+}
